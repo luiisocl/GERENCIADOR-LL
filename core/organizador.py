2940536626f7615abc129abbf5eafdf47b34eebe
luiisocl <lg3545396@gmail.com>
2026-05-06 21:26:46 -0300

    Adicionado informações do autor nos arquivos

---
+++ core/organizador.py
@@ -0,0 +1,9 @@
+"""
+Gerenciador LL
+==============
+Autor: Luis Leal
+GitHub: github.com/luiisocl/GERENCIADOR-LL
+Versão: 1.0.0
+Descrição: Gerenciador de arquivos inteligente com IA integrada
+Licença: Todos os direitos reservados © 2026 Luis Leal
+"""
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Gerenciador LL
+==============
+Autor: Luis Leal
+GitHub: github.com/luiisocl/GERENCIADOR-LL
+Versão: 1.0.0
+Descrição: Gerenciador de arquivos inteligente com IA integrada
+Licença: Todos os direitos reservados © 2026 Luis Leal
+"""
+
 import os
 import datetime 
 
