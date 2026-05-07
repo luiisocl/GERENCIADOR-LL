88809fe54ed27211dc7184f48e2c254457d006c0
luiisocl <lg3545396@gmail.com>
2026-05-06 21:02:25 -0300

    Início do Projeto Gerenciador LL

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import datetime 
+
+pasta = "C:/Users/Luis Guilherme/Documents"
+
+arquivos  = os.listdir(pasta)
+
+print("=== Gerenciador LL ===")
+print(f"Arquivos encontrados: {len(arquivos)}")
+print("-----------------------------")
+
+
+for arquivo in arquivos:
+    caminho_completo = os.path.join(pasta, arquivo)
+
+    if os.path.isfile(caminho_completo):
+        tamanho = os.path.getsize(caminho_completo)
+        tamanho_mb = round(tamanho /1024/ 1024, 2)
+
+        data_modificacao = os.path.getmtime(caminho_completo)
+        data= datetime.datetime.fromtimestamp(data_modificacao)
+
+        data_formatada = data.strftime("%d/%m/%Y")
+
+
+        print(f"Arquivo: {arquivo} | Tamanho: {tamanho_mb} MB | Data de Modificação: {data_formatada}")
